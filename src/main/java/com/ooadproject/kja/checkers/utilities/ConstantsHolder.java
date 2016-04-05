38c29f67041f214f3be2ad6a8258483996a91744
Alex Tzinov <alex.tzinov@gmail.com>
2016-04-05 11:29:51 -0600

    added this file so that we can store all of our constants in one place and just reference them instead of copying pasting them across the app

---
+++ src/main/java/com/ooadproject/kja/checkers/utilities/ConstantsHolder.java
@@ -0,0 +1,17 @@
+package com.ooadproject.kja.checkers.utilities;
+
+
+public class ConstantsHolder {
+
+  private ConstantsHolder()
+  {
+  }
+
+	public static final int RED = 1;
+	public static final int BLACK = 2;
+	public static final int EMPTY = 0;
+
+	public static final int BOARD_SIZE = 8;
+
+}
+
