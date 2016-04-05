4d820cafd84400c856a2e972ebd1a41d253ffa9f
Alex Tzinov <alex.tzinov@gmail.com>
2016-04-05 11:33:23 -0600

    made it a singleton, added getsize method, color constants now referenced from const file, removed print grid method, populate grid no longer returns grid

--- src/main/java/com/ooadproject/kja/checkers/business_logic/BoardLogic.java
+++ src/main/java/com/ooadproject/kja/checkers/business_logic/BoardLogic.java
@@ -1,83 +1,66 @@
 package com.ooadproject.kja.checkers.business_logic;
 
 import com.ooadproject.kja.checkers.models.*;
+import com.ooadproject.kja.checkers.utilities.*;
 
 
 public class BoardLogic {
-	
-	private final int RED = 1;
-	private final int BLACK = 2;
-	private final int EMPTY = 0;
-	public BoardLogic()
-	{
 
-	}
+  private static BoardLogic boardLogic;
 
-	public Board populateGrid(Board checkersBoard)
-	{
+  private BoardLogic()
+  {
+  }
 
-		for (int row = 0; row < 8; row++)
-		{
-			for (int col = 0; col < 8; col++)
-			{
-				if (row % 2 != col % 2)
-				{
-					if (row < 3)
-						checkersBoard.piecesGrid[row][col] = new Piece(RED, col, row);
-					else if (row > 4)
-						checkersBoard.piecesGrid[row][col] = new Piece(BLACK, col, row);
-					else
-						checkersBoard.piecesGrid[row][col] = new Piece(EMPTY, col, row);
-				} else
-				{
-					checkersBoard.piecesGrid[row][col] = new Piece(EMPTY, col, row);
-				}
-			}
-		}
-	
-		return checkersBoard;
+  public static BoardLogic getBoardLogic(){
+    System.out.println("Getting your board printer");
+    if (boardLogic == null)
+        boardLogic = new BoardLogic();
+    return boardLogic;
+  }
 
+  public int getGridSize(Board checkersBoard)
+  {
+    return checkersBoard.piecesGrid[0].length;
+  }
+
+  public void populateGrid(Board checkersBoard)
+  {
+    int boardSize = getGridSize(checkersBoard);
+    for (int row = 0; row < boardSize; row++)
+    {
+      for (int col = 0; col < boardSize; col++)
+      {
+        if (row % 2 != col % 2)
+        {
+          if (row < 3)
+            checkersBoard.piecesGrid[row][col] = new Piece(ConstantsHolder.RED, row, col);
+          else if (row > 4)
+            checkersBoard.piecesGrid[row][col] = new Piece(ConstantsHolder.BLACK, row, col);
+          else
+            checkersBoard.piecesGrid[row][col] = new Piece(ConstantsHolder.EMPTY, row, col);
+        } else
+        {
+          checkersBoard.piecesGrid[row][col] = new Piece(ConstantsHolder.EMPTY, row, col);
+        }
       }
+    }
+  }
   public Piece getPiece(Board checkersBoard, int col, int row)
   {
-	  return checkersBoard.piecesGrid[col][row];
+    return checkersBoard.piecesGrid[col][row];
   }
 
   //Fix this
   public Board movePiece(Board checkersBoard, Move move)
   {
-	  checkersBoard.piecesGrid[move.toCol][move.toRow].setColor(checkersBoard.piecesGrid[move.fromCol][move.fromRow].getColor());
-	  //checkersBoard.piecesGrid[move.toCol][move.toRow] = checkersBoard.piecesGrid[move.fromCol][move.fromRow];
-	  checkersBoard.piecesGrid[move.fromCol][move.fromRow].setColor(EMPTY);
-	  return checkersBoard;
+    checkersBoard.piecesGrid[move.toCol][move.toRow].setColor(checkersBoard.piecesGrid[move.fromCol][move.fromRow].getColor());
+    //checkersBoard.piecesGrid[move.toCol][move.toRow] = checkersBoard.piecesGrid[move.fromCol][move.fromRow];
+    checkersBoard.piecesGrid[move.fromCol][move.fromRow].setColor(ConstantsHolder.EMPTY);
+    return checkersBoard;
   }
 
   public boolean validateMove(Board checkersBoard, Move move){
-	 return true; 
+    return true; 
   }
-  
-  public void printGrid(Board checkersBoard){
-	  int color;
-	  for (int row = 0; row < 8; row++)
-		{
-			for (int col = 0; col < 8; col++)
-			{
-				color = checkersBoard.piecesGrid[col][row].getColor();
-				switch(color){
-					case BLACK: 
-						System.out.print("" + row + "," + col + " equals black | ");
-						break;
-					case RED: 
-						System.out.print("" + row + "," + col + " equals red | ");
-						break;
-					case EMPTY: 
-						System.out.print("" + row + "," + col + " equals empty | ");
-						break;	
-				}	
-			}
-			System.out.println("");
-		}
-  }
-
-  
 }
