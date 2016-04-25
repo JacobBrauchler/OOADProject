31396f4e37858a62bdd20b75953ae397f98132e2
Jacob Brauchler <jacobbrauchler@Jacobs-MacBook-Pro.local>
2016-04-25 10:29:26 -0600

    board listener creates a move object

--- src/main/java/com/ooadproject/kja/checkers/views/BoardListener.java
+++ src/main/java/com/ooadproject/kja/checkers/views/BoardListener.java
@@ -1,33 +1,45 @@
 package com.ooadproject.kja.checkers.views;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 
+import com.ooadproject.kja.checkers.models.Board;
+import com.ooadproject.kja.checkers.models.Move;
 import com.ooadproject.kja.checkers.utilities.ConstantsHolder;
 import com.ooadproject.kja.checkers.views.*;
+import com.ooadproject.kja.checkers.business_logic.*;
 
-public class BoardListener implements MouseListener{
+public class BoardListener implements MouseListener, ActionListener{
 	int click = 0;
+	Move move;
+	int fromcol;
+	int fromrow;
+	int torow;
+	int tocol;
+	private static Board checkersBoard;
 
 	public void mouseClicked(MouseEvent e) {
 		if (click == 0){
 		// TODO Auto-generated method stub
-			int fromrow=(e.getY()-ConstantsHolder.RowSpacing)/ConstantsHolder.multiplierColRow;
-			int fromcol=(e.getX()-ConstantsHolder.ColSpacing)/ConstantsHolder.multiplierColRow;
+			fromrow=(e.getY()-ConstantsHolder.RowSpacing)/ConstantsHolder.multiplierColRow;
+			fromcol=(e.getX()-ConstantsHolder.ColSpacing)/ConstantsHolder.multiplierColRow;
 			System.out.println("fromrow " + fromrow+", fromcol "+fromcol);
 			click ++;
 		}
 		else if (click == 1){
-			int torow=(e.getY()-ConstantsHolder.RowSpacing)/ConstantsHolder.multiplierColRow;
-			int tocol=(e.getX()-ConstantsHolder.ColSpacing)/ConstantsHolder.multiplierColRow;
+			torow=(e.getY()-ConstantsHolder.RowSpacing)/ConstantsHolder.multiplierColRow;
+			tocol=(e.getX()-ConstantsHolder.ColSpacing)/ConstantsHolder.multiplierColRow;
 			System.out.println("torow " + torow+", tocol "+tocol);
 			click = 0;
 			
 			
 		}
-        
+		
+		move = new Move(fromrow, fromcol, torow, tocol);
 			
 	}
 
@@ -49,10 +61,12 @@ public class BoardListener implements MouseListener{
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
-	 */
 	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
