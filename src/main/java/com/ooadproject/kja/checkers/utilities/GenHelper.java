c048c035e7390c2dc27ba1a6782cea8abbfdc06c
Alex Tzinov <alex.tzinov@gmail.com>
2016-04-25 23:32:57 -0600

    added helper for getting screen size

--- src/main/java/com/ooadproject/kja/checkers/utilities/GenHelper.java
+++ src/main/java/com/ooadproject/kja/checkers/utilities/GenHelper.java
@@ -1,8 +1,9 @@
 package com.ooadproject.kja.checkers.utilities;
+import java.awt.Toolkit.*;
 public class GenHelper {
-	private final int RED = 1;
-	private final int BLACK = 2;
-	private final int EMPTY = 0;
+  private final int RED = 1;
+  private final int BLACK = 2;
+  private final int EMPTY = 0;
   private static GenHelper genHelper;
 
   private GenHelper()
@@ -11,7 +12,7 @@ public class GenHelper {
 
   public static GenHelper getGenHelper(){
     if (genHelper == null)
-        genHelper = new GenHelper();
+      genHelper = new GenHelper();
     return genHelper;
   }
 
@@ -19,13 +20,20 @@ public class GenHelper {
   public String pieceColorConverter(int color)
   {
     switch (color) {
-    case RED:
-      return "RED";
-    case BLACK:
-      return "BLACK";
-    case EMPTY:
-      return "EMPTY";
+      case RED:
+        return "RED";
+      case BLACK:
+        return "BLACK";
+      case EMPTY:
+        return "EMPTY";
     }
     return "oops";
+  }
+  public static int GetScreenWorkingWidth() {
+    return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+  }
+
+  public static int GetScreenWorkingHeight() {
+    return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
   }
 }
